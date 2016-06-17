3b9e0824061f3eb491c516eb1f0d6eb964a9b8e1
Daniel Cavalli <daniel.cavalli@outlook.com>
2016-06-17 12:28:26 -0300

    Dynamic GridLayout

--- InsightProjectAndroid/app/src/main/java/com/example/danielcavalli/project_insight/MainActivity.java
+++ InsightProjectAndroid/app/src/main/java/com/example/danielcavalli/project_insight/MainActivity.java
@@ -2,27 +2,69 @@ package com.example.danielcavalli.project_insight;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.SharedPreferences.Editor;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+
+import carbon.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
-
-    SharedPreferences.Editor editor;
-    SharedPreferences s;
-
+    int num = 0;
+    //SharedPreferences.Editor editor;
+    //SharedPreferences s;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teste);
+        setContentView(R.layout.pscreen);
 
-        s = this.getSharedPreferences("com.example.danielcavalli.project_insight", Activity.MODE_PRIVATE);
-        editor = s.edit();
+        //s = this.getSharedPreferences("com.example.danielcavalli.project_insight", Activity.MODE_PRIVATE);
+        //editor = s.edit();
     }
 
-    public void showDialog(View v){
+    public void changeScreen(View v){
+        //setContentView(R.layout.pscreen);
+    }
 
+    public void addProject(View v){
+        Button lb = (Button) findViewById(R.id.button_layout);
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.grid);
+
+        EditText edit = (EditText)findViewById(R.id.project_name);
+
+        if(num < gridLayout.getRowCount() *gridLayout.getColumnCount()  &&  edit.getText().toString() != "") {
+            Log.d("MainActivity", gridLayout.getColumnCount() + " || " + gridLayout.getRowCount());
+            Button b = new Button(this);
+            b.setText(edit.getText().toString());
+            b.setMinWidth(gridLayout.getWidth() / gridLayout.getRowCount());
+            b.setMinHeight(gridLayout.getHeight() / gridLayout.getColumnCount());
+            b.setBackgroundColor(Color.WHITE);
+            b.setTextColor(Color.parseColor("#529e90"));
+            num++;
+            Log.d("MainActivity", b.toString());
+            gridLayout.addView(b);
+            edit.setText("");
+            ((InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE))
+                    .toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0);
+        }
+        findViewById(R.id.namingView).setVisibility(View.GONE);
+    }
+
+    public void showDialog(View v) {
+        View d = findViewById(R.id.namingView);
+        d.setVisibility(View.VISIBLE);
+    }
+
+    public void hideDialog(View v){
+        View d = findViewById(R.id.namingView);
+        d.setVisibility(View.GONE);
     }
 
 }
