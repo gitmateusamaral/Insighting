922dfb580a34caff6117e63565162a606169d757
victorffernandes <victorfernandes.nave@gmail.com>
2016-07-08 14:00:26 -0400

    Layout Inflater and Scroll View

--- InsightProjectReborn/app/src/main/java/com/example/facebook/insighting/MainActivity.java
+++ InsightProjectReborn/app/src/main/java/com/example/facebook/insighting/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.facebook.insighting;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addCardView(View v){
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        ViewGroup gridlayout = (ViewGroup) findViewById(R.id.grid);
+        inflater.inflate(R.layout.cardview, gridlayout);
     }
 
 }
