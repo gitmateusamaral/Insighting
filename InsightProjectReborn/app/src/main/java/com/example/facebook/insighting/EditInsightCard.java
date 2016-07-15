e4392816f296c0faeff91e8eb1c9ded8b27962bb
Matheus Souza <mathsouza64@gmail.com>
2016-07-15 15:06:21 -0300

    Started edit insight card layout

--- InsightProjectReborn/app/src/main/java/com/example/facebook/insighting/EditInsightCard.java
+++ InsightProjectReborn/app/src/main/java/com/example/facebook/insighting/EditInsightCard.java
@@ -1,13 +1,23 @@
 package com.example.facebook.insighting;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditInsightCard extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_edit_insight_card);
+        Typeface face = Typeface.createFromAsset(getAssets(),"fonts/Quicksand-Regular.ttf");
+        EditText et = (EditText) findViewById(R.id.ic_textTitle);
+        EditText et2 = (EditText) findViewById(R.id.ic_textDescription);
+        et.setTypeface(face);
+        et2.setTypeface(face);
     }
 }
